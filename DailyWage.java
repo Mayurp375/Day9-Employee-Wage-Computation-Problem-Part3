9c09931749ed860f47c4f7506636a45c02527e66
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-13 22:40:20 +0530

    for 20 days

--- DailyWage.java
+++ DailyWage.java
@@ -6,24 +6,27 @@ public class DailyWage extends Presenty {
     static void check() {
         int perHour = 20;
         int dailyHour = 8;
+        int totalHour =0;
         Random random = new Random();
-        int attendance = random.nextInt(3);
-        switch (attendance) {
-            case 1:
-                perHour = 20;
-                dailyHour = 8;
-                System.out.println("present");
-                break;
-            case 2:
-                System.out.println("absent");
-                break;
-            default:
-                perHour = 20;
-                dailyHour = 4;
-                System.out.println("part time");
+        for (int i = 0; i <= 20; i++) {
+            int attendance = random.nextInt(3);
+            switch (attendance) {
+                case 1:
+                    perHour = 20;
+                    dailyHour = 8;
+                    System.out.println("present");
+                    break;
+                case 2:
+                    System.out.println("absent");
+                    break;
+                default:
+                    perHour = 20;
+                    dailyHour = 4;
+                    System.out.println("part time");
+            }
+           totalHour = totalHour + dailyHour;
         }
-        int dailyWage = perHour * dailyHour;
-        int monthly = dailyWage *20;
-        System.out.println("daily wage is : " + monthly);
+        int monthly = totalHour * 20;
+        System.out.println("monthly wage is : " + monthly);
     }
 }
