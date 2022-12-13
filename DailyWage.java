c10edb0de39dfb3c514360e4d0280dc26d516991
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-13 22:32:28 +0530

    switch case

--- DailyWage.java
+++ DailyWage.java
@@ -8,16 +8,19 @@ public class DailyWage extends Presenty {
         int dailyHour = 8;
         Random random = new Random();
         int attendance = random.nextInt(3);
-        if (attendance == 1) {
-            perHour = 20;
-            dailyHour = 8;
-            System.out.println("present");
-        } else if(attendance == 2){
-            System.out.println("absent");
-        }else{
-            perHour = 20;
-            dailyHour = 4;
-            System.out.println("part time");
+        switch (attendance) {
+            case 1:
+                perHour = 20;
+                dailyHour = 8;
+                System.out.println("present");
+                break;
+            case 2:
+                System.out.println("absent");
+                break;
+            default:
+                perHour = 20;
+                dailyHour = 4;
+                System.out.println("part time");
         }
         int dailyWage = perHour * dailyHour;
         System.out.println("daily wage is : " + dailyWage);
