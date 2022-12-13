f70232598d6c4f3e32ff33ea158dc5c2c5f11052
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-13 22:14:25 +0530

    main fiile

---
+++ Main.java
@@ -0,0 +1,7 @@
+package classProblemByAmolMateSir.Day9.EmployeeWage;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome EmployeeWage ");
+    }
+}
